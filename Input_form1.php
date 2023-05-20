d38563c0c73b65970a6b8f9b59e53874eeb8e2e8
chetan-kasar <116253410+chetan-kasar@users.noreply.github.com>
2023-05-20 11:39:28 +0530

    Add files via upload

---
+++ Input_form1.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+    <head>
+        <title>FIrst Input</title>
+    </head>
+
+    <body>
+        <form action = "input_form.php" method="post">
+            <h3>Enter Your Name</h3>
+            <input type = "text" name = "name">
+
+            <h3>Enter Your Password</h3>
+            <input type = "text" name = "pass">
+
+            <input type = "submit">
+        </form>
+    </body>
+</html>
